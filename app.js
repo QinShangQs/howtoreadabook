b908126236f221f99644f83e67f0c7ad31a74d34
liziqiang <liziqiang.qs@outlook.com>
2017-05-13 09:58:03 +0800

    test

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //app.js
 App({
+ data:{  
+    appid:'wx29aa31ee54d6feb3',
+    appsecret:'41ba95339b1a4b2c73f6e92b5e98f0c2',
+ },
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
@@ -13,11 +17,23 @@ App({
     }else{
       //调用登录接口
       wx.login({
-        success: function (lres) {
-          console.info(lres);
+        success: function (res) {
+          //console.info(res);
+          if(res.code){
+            wx.request({
+                url: 'https://api.weixin.qq.com/sns/jscode2session?appid='+getApp().data.appid+'&secret='+getApp().data.appsecret+'&js_code='+res.code+'&grant_type=authorization_code',
+
+                success: function (response) {
+                 // console.log(response.openid)
+                  //console.log(response);
+                }
+              })
+          }
+
+
           wx.getUserInfo({
             success: function (res) {
-              console.info(res);
+             // console.info(res);
               that.globalData.userInfo = res.userInfo
               typeof cb == "function" && cb(that.globalData.userInfo)
             }
